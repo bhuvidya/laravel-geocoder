ac195bd39cdd632179e5c152ac5c4c097eaabf78
bhu Boue vidya <bhuvidya@gmail.com>
2017-12-01 00:13:10 +1100

    Added a "geocode" method, which geocodes an address using Google Maps API

--- src/Geocoder.php
+++ src/Geocoder.php
@@ -11,6 +11,7 @@ use Log;
 class Geocoder
 {
     public static $lastResponse;
+    public static $lastStatus;
 
     protected static $httpClient;
 
@@ -83,6 +84,90 @@ class Geocoder
         }
 
         return [ 'lat' => $info->latitude, 'lng' => $info->longitude ];
+    }
+
+
+    /**
+     * geocode an address using the Google Maps API
+     *
+     * @param string $addr - the address to geocode
+     * @param bool $orig - if true then stash the original returned data
+     * @return object | false
+     */
+    public static function geocode($addr, $orig = false)
+    {
+        $url = sprintf(
+            'https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false&key=%s',
+            urlencode($addr),
+            config('geocoder.google_maps_api_key')
+        );
+
+        $headers = [ 'headers' => [ 'Accept' => 'application/json' ] ];
+
+        while (true) {
+            static::$lastResponse = $response = static::getHttpClient()->get($url, $headers);
+
+            if (!$response) {
+                return false;
+            }
+
+            if (!$json = @json_decode(iconv('ISO-8859-1', 'UTF-8', $response->getBody()))) {
+                return false;
+            }
+
+            $status = static::$lastStatus = $json->status;
+
+            if ($status == 'OK') {
+                $info = new stdClass();
+
+                // Successful geocode
+                // Format: Longitude, Latitude, Altitude
+                $best = $json->results[0];
+                $info->address = $best->formatted_address;
+
+                $geo = $best->geometry;
+                $info->center = (object) array(
+                    'lat' => $geo->location->lat,
+                    'lng' => $geo->location->lng
+                );
+
+                if ($geo->bounds) {
+                    $info->box = (object) array(
+                        'north' => $geo->bounds->northeast->lat,
+                        'south' => $geo->bounds->southwest->lat,
+                        'east' => $geo->bounds->northeast->lng,
+                        'west' => $geo->bounds->southwest->lng
+                    );
+                }
+
+                $bits = $best->address_components;
+                foreach ($bits as $bit) {
+                    if ($bit->types && $bit->types[0] == 'country') {
+                        $info->country = $bit->long_name;
+                        $info->countryCode = $bit->short_name;
+                    }
+                    if ($bit->types && $bit->types[0] == 'administrative_area_level_1') {
+                        $info->province = $bit->short_name;
+                    }
+                    if ($bit->types && $bit->types[0] == 'postal_code') {
+                        $info->postal_code = $bit->long_name;
+                    }
+                }
+
+                if ($orig) {
+                    $info->_orig = $json;
+                }
+
+                return $info;
+            } elseif ($status == 'OVER_QUERY_LIMIT') {
+                // delay then try again
+                usleep(config('geocoder.rate_limit_delay_secs') * 1000000);
+            } elseif ($status == 'UNKNOWN_ERROR') {
+                return false;
+            } else {
+                return false;
+            }
+        }
     }
 
 
